41e95273bb291174d90111edbb567b088feb9326
Daiana <daianachichotky@gmail.com>
2025-11-23 17:39:50 +0100

    adding game palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,27 @@
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); //process.argv[2] is the text that the user writes after the 'node palindrome.js'
 
-if (args.length !== 3) {
-  console.error('Please provide exactly 2 numbers and 1 operator (+, -, *, /');
+const phrase = args.join(' ').toLowerCase(); //join all in just 1 string and put all in lower case: ["Hola Que tal?"] --> ["hola que tal?"]
+const word = phrase.split(' ').join(''); // remove spaces and join removing spaces: ["hola que tal?"] --> ["hola", "que", "tal", "?"] --> "holaquetal?""
+
+if (!word || word === '') {
+  console.log('Please enter text.');
   return;
 }
 
-const num1 = parseFloat(args[0]);
-const num2 = parseFloat(args[1]);
-const operator = args[2];
+// Resto de tu código para chequear palíndromo
+
+let isPalindrome = true;
+
+for (let index = 0; index < word.length / 2; index++) {
+  //we only check the half because we compare the 1st with the last and so on
+  if (word[index] !== word[word.length - 1 - index]) {
+    isPalindrome = false;
+    break;
+  }
+}
+
+if (isPalindrome) {
+  console.log(`"${word}" is a palindrome!`);
+} else {
+  console.log(`"${word}" is NOT a palindrome`);
+}
