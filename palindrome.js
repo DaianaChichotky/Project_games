6af1fd60e0fb9289a37bb4ed63b785454d633731
Daiana <daianachichotky@gmail.com>
2025-11-29 11:15:31 +0100

    removing accent from letters

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,18 @@
 const args = process.argv.slice(2);
 
-const word = args.join('').toLowerCase(); //join all in just 1 string and put all in lower case.
+const sentence = args.join(' '); //join all in just 1 string separating each word by empty space
+const word = args
+  .join('')
+  .toLowerCase()
+  .normalize('NFD') // to separate letters from the accent
+  .replace(/[\u0300-\u036f]/g, ''); // to remove the accent
 
 if (!word || word === '') {
   console.log('Please enter text.');
   return;
 }
 
-let isPalindrome = true;
+let isPalindrome = true; //I assume is true until I demonstrate is false
 
 for (let index = 0; index < word.length / 2; index++) {
   //we only check the half because we compare the 1st with the last and so on
@@ -18,7 +23,7 @@ for (let index = 0; index < word.length / 2; index++) {
 }
 
 if (isPalindrome) {
-  console.log(`"${word}" is a palindrome!`);
+  console.log(`"${sentence}" is a palindrome!`);
 } else {
-  console.log(`"${word}" is NOT a palindrome`);
+  console.log(`"${sentence}" is NOT a palindrome`);
 }
